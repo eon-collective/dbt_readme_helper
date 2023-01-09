91b93128e8c0bf71166c02f400302460f748101c
williamdst <schdwilliams@gmail.com>
2023-01-09 10:12:13 +0000

    fix: Correct the name of the fully qualified macro

--- macros/log_results_csv.sql
+++ macros/log_results_csv.sql
@@ -2,13 +2,13 @@
 
     {% if execute %}
   			{%- do log("Logging sources csv", true) -%}
-        {{ dbt_readme_helper.log_sources(graph) }}
+        {{ dbt_readme_logger.log_sources(graph) }}
 
         {% do log("Logging seeds csv", true) %}
-        {{ dbt_readme_helper.log_seeds(graph) }}
+        {{ dbt_readme_logger.log_seeds(graph) }}
 
         {% do log("Logging models csv", true) %}
-        {{ dbt_readme_helper.log_models(graph) }}
+        {{ dbt_readme_logger.log_models(graph) }}
     {% endif %}
   
 {%- endmacro %}
